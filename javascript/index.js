ef642b36d25533ce7f0f96b4ed2bb3ed81817111
Maxime BAUSSANT <baussant.maxime62@gmail.com>
2025-04-02 11:34:50 +0200

    ajout fond animé avec particule

--- javascript/index.js
+++ javascript/index.js
@@ -125,6 +125,57 @@ function filterProjects(category) {
             proj.classList.remove("show");
         }
     });
+
+    // Animation de fond
+    const canvas = document.getElementById("particleCanvas");
+        const ctx = canvas.getContext("2d");
+        
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        
+        let particlesArray = [];
+        const numberOfParticles = 100;
+
+        class Particle {
+            constructor() {
+                this.x = Math.random() * canvas.width;
+                this.y = Math.random() * canvas.height;
+                this.size = Math.random() * 3 + 1;
+                this.speedX = (Math.random() * 0.5 - 0.25); 
+                this.speedY = (Math.random() * 0.5 - 0.25);
+                this.opacity = Math.random() * 0.1 + 0.05;
+            }
+            update() {
+                this.x += this.speedX;
+                this.y += this.speedY;
+                if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+                if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+            }
+            draw() {
+                ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
+                ctx.beginPath();
+                ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+                ctx.fill();
+            }
+        }
+
+        function init() {
+            for (let i = 0; i < numberOfParticles; i++) {
+                particlesArray.push(new Particle());
+            }
+        }
+
+        function animate() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            particlesArray.forEach(particle => {
+                particle.update();
+                particle.draw();
+            });
+            requestAnimationFrame(animate);
+        }
+
+        init();
+        animate();
 }
 
 // Afficher tous les projets au chargement de la page
